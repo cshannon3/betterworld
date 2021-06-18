952f81407d89fa869c914dde02610c34f62a7db7
cshannon3 <conreshan@gmail.com>
2021-06-18 11:31:19 -0400

    at a glance formatting

--- src/containers/3_Project_Page/Modules/AtAGlanceModule/AtAGlanceModule.jsx
+++ src/containers/3_Project_Page/Modules/AtAGlanceModule/AtAGlanceModule.jsx
@@ -5,80 +5,115 @@ import * as styles from '../../../../styles/sharedStyles';
 import docIcon from 'assets/Landing/google-docs.png';
 import sheetsIcon from 'assets/Landing/google-sheets.png';
 
-export default function AtAGlanceModule() {
+//goal of at a glance: people photos, resource # and repsonisbilities
 
-
+export default function AtAGlanceModule({ projectData }) {
+  const formatDate = (d) =>{
+    return d.substring(0, d.length-13)
+  }
   
-   return ( <AtAGlanceBox>
-        <styles.GreenTitleBar>At A Glance Box</styles.GreenTitleBar>
-        <DivContainer >
-          <InternalDiv>
-            <LargeText>5</LargeText>
-            <img src={docIcon}/>
-            <SmallText>Google Docs</SmallText>
-          </InternalDiv>
-          <InternalDiv>
-            <LargeText>3</LargeText>
-            <img src={sheetsIcon}/>
-            <SmallText>Google Sheets</SmallText>
-          </InternalDiv>
-          <InternalDiv>
-            <LargeText>3/21/21</LargeText>
-            <SmallText>Last Updated</SmallText>
-          </InternalDiv>
-          <FinalInternalDiv>
-            <LargeText>3\71</LargeText>
-            <SmallText>Edits This Week</SmallText>
-          </FinalInternalDiv>
-        </DivContainer>
-    </AtAGlanceBox>);
+  return (
+    <AtAGlanceBox>
+      <styles.GreenTitleBar>At A Glance Box</styles.GreenTitleBar>
+      <Column>
+        <Row>
+          <Box>
+            <SubtitleBold>{`Contributors (${projectData["contributors"] && projectData["contributors"].length})`}</SubtitleBold>
+            <ul>
+              {projectData["contributors"] &&
+                projectData["contributors"].map((data) => (
+                  <li>{data.name}</li>
+                ))}
+            </ul>
+          </Box>
+          <Box>
+          <SubtitleBold>Target Date</SubtitleBold>
+          <TargetDateText>{formatDate(projectData["end_date"])}</TargetDateText>
+          </Box>
+        </Row>
+
+        <Row>
+          <Box>
+           <SubtitleBold> Useful Links</SubtitleBold>
+            <ResourceBox>
+              {projectData["resources"] &&
+                projectData["resources"].map((data) => (
+                  <ArtifactLink>
+                    <a href={data.url} target="_blank">
+                      {data.name}
+                    </a>
+                  </ArtifactLink>
+                ))}
+            </ResourceBox>
+          </Box>
+          <Box>
+         <SubtitleBold> Schedule</SubtitleBold>
+          <ul>
+              {projectData["schedule"] &&
+                projectData["schedule"].map((data) => (
+                  <li>{data}</li>
+                ))}
+            </ul>
+
+          </Box>
+        </Row>
+      </Column>
+    </AtAGlanceBox>
+  );
 }
 
 const AtAGlanceBox = styled.div`
-  display: grid;
-  grid-area: 1 / 3 / span 1 / span 2;
-  background: #FFFFFF;
+  height:100%;
+  width:50%;
+  background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 3px;
-`
-const DivContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-`
-const InternalDiv = styled.div`
-  max-width: 150px;
-  margin: 10px;
-  padding: 5px;
-  border-right: dashed 1px lightgrey;
-  text-align: center;
-`
-const FinalInternalDiv = styled.div`
-  max-width: 150px;
-  margin: 10px;
-  padding: 5px;
-  text-align: center;
-`
 
-//Shared Style: same as DateTitle from project detail
-const LargeText = styled.h2`
-    font-family: Baloo 2;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 40px;
-    line-height: 70px;
-    display: inline-block;
-    align-items: center;
-    letter-spacing: -0.02em;
-    color: black;
-`;
-const SmallText = styled.h2`
-    font-family: Baloo 2;
-    font-style: normal;
-    font-weight: 800;
-    font-size: 16px;
+  .col {
     display: flex;
-    align-items: center;
-    letter-spacing: -0.02em;
-
-    color: #000000;
+    justify-content: space-evenly;
+  }
+  li {
+    margin-left: 20px;
+  }
 `;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50%;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 38px);
+  width: 100%;
+`;
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  padding: 10px;
+`;
+
+const ResourceBox = styled.div`
+  padding-left: 20px;
+`;
+
+const ArtifactLink = styled.p`
+  font-size: 16px;
+  line-height: 18px;
+  align-items: center;
+`;
+
+const SubtitleBold= styled.h2`
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+const TargetDateText = styled.h2`
+  font-weight: bold;
+  font-size: 30px;
+  padding: 10px 0px;
+`;
+
