91741de81ec825594d109aa74c40750f7edb08a9
Connor Shannon <33521708+cshannon3@users.noreply.github.com>
2021-10-05 12:43:51 -0400

    Delete src/old directory

--- src/old/ProjectPage/Modals/index.js
+++
@@ -1,3 +0,0 @@
-export {default as LadderModal} from './LadderModal/LadderModal';
-export {default as HelpRequestsModal} from './HelpRequestsModal';
-export {default as EditProjectInfoModal} from './EditProjectInfoModal';
--- src/old/ProjectPage/Modules/modules.js
+++
@@ -1,7 +0,0 @@
-
-
-export {default as AtAGlanceModule} from '../../../components/AtAGlanceModule';
-//export {default as HelpWantedModule} from './HelpWantedModule/HelpWantedModule';
-export {default as LadderModule} from './LadderModule';
-export {default as ProjectInfoModule} from './ProjectInfoModule';
-
